5736c8807e7882bf08ade9b80ab036ca03c5899a
waheed <w.faiza@gmail.com>
2024-10-16 17:31:40 +0200

    .

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader, TextLoader #, MarkdownLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFacePipeline
-from langchain.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFacePipeline
+from langchain_community.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from openai import OpenAI
