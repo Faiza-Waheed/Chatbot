dc67d71fb7a16aabbecf0badf419f4fb0cbfeb53
waheed <w.faiza@gmail.com>
2024-10-16 17:24:49 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader, TextLoader, MarkdownLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, MarkdownLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import HuggingFacePipeline
